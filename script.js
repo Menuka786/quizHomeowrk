751c52df14d799f0d83f61804c78d562b9bcd40b
Menuka Gusain <menukagusain@gmail.com>
2020-12-04 18:38:29 -0500

    updated

--- script.js
+++ script.js
@@ -1,31 +1,40 @@
-var startButton = document.querySelector('button');
+console.log("App connecting");
+
+
+var startButton = document.getElementsByClassName("btn btn-danger");
+
+
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+startButton.addEventListener("click", startTimer) 
+    var fiveMinutes = 60 * 5,
+    display = document.querySelector('#time');
+    startTimer(fiveMinutes, display);
 
 
 
- function startTimer (duration, display) {
-     var timer = duration, minutes, seconds;
-     setInterval(function (){
-         minutes = parseInt(timer / 60,10);
-         second = parseInt(timer % 60,10);
-         + minutes : minutes;
-         seconds = seconds < 10 ? "0" + seconds : seconds;
-         display.textContent = minutes + ":" + seconds;
+/*window.onload = function () {
+var fiveMinutes = 60 * 5,
+display = document.querySelector('#time');
+startTimer(fiveMinutes, display);
 
-         if (--timer < 0) {
-             timer = duration;
-         }
-     }, 1000);
- }
- 
- window.onload = function () {
-     var fiveMinutes = 60 * 5,
-         display = document.querySelector('#time');
-     startTimer(fiveMinutes, display);
- };
+}
 
-
- startButton.addEventListener('click', startTimer);
- 
 
  var quizQuestions = [
     
