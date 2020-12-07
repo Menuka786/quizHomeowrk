ad91094d29fdc223e36164e80f2dc9fe9b4c8788
Menuka Gusain <menukagusain@gmail.com>
2020-12-06 22:34:40 -0500

    upated

---
+++ question.js
@@ -0,0 +1,58 @@
+
+ var questions = [
+    
+    {
+"question": "Who wrote first version of HTML?",
+"option1": "Tim Berners Lee",
+"option2": "Mike Berners-Lee",
+"option3": "Larry page",
+"option4": "Tim Ben",
+"answer": "1",
+    
+},
+{
+    "question": "Who invented JavaScript?",
+    "option1": "Tim Berners Lee",
+    "option2": "Mike Berners-Lee",
+    "option3": "Brendan Eich",
+    "option4":"Sheryl Sandb",
+    "answer": "3"
+
+
+},
+ { 
+    "question": " What is the full form of CSS?",
+ 
+"option1":"Cascading Style Sheets",
+ "option2":"Cascading Style System",
+"option3": "Customer Stye Sheets",
+"option4": "Customer System Style",
+"answer" : "1"
+
+ },
+ { 
+    "question": " Inside which HTML element do we put the JavaScript?",
+ 
+"option1":"myString",
+ "option2": "</script>",
+"option3": "<js>",
+"option4": "string",
+"answer" : "2"
+
+ },
+ { 
+    "question": " Is JavaScript same as Java?",
+ 
+"option1":"True",
+ "option2":"False",
+"option3": "Both are same",
+"option4": "Not sure",
+"answer" : "2"
+
+ },
+]
+
+
+
+
+
--- script.js
+++ script.js
@@ -4,6 +4,7 @@ console.log("App connecting");
 var startButton = document.getElementsByClassName("btn btn-danger");
 
 
+
 function startTimer(duration, display) {
     var timer = duration, minutes, seconds;
     setInterval(function () {
@@ -30,10 +31,97 @@ startButton[0].addEventListener("click", function(){
     startTimer(fiveMinutes, display);
 
 })
+
+var currentQuestion = 0;
+var score = 0;
+var totQuestions = question.length;
+
+var container = document.getElementById("quizContainer");
+var questionEl = document.getElementById("question");
+var opt1 = document.getElementById("opt1");
+var opt2 = document.getElementById("opt2");
+var opt3 = document.getElementById("opt3");
+var opt4 = document.getElementById("opt4");
+var nextButton = document.getElementById("nextButton");
+var resultCont = document.getElementById("result");
+
+function loadQuestion(questionIndex) {
+  var q = questions[questionIndex];
+  questionEl.textContent = (questionIndex + 1) + ". " + q.question;
+  opt1.textContent = q.option1;
+  opt2.textContent = q.option2;
+  opt3.textContent = q.option3;
+  opt4.textContent = q.option4;
+};
+
+function loadNextQuestion() {
+  var selectedOption = document.querySelector("input[type=radio]:checked");
+  if (!selectedOption) {
+    alert("Please select your answer!");
+    return;
+  }
+
+  var answer = selectedOption.value;
+  if (questions[currentQuestion].answer == answer) {
+    score += 10;
+  }
+
+  selectedOption.checked = false;
+  currentQuestion++;
+  if (currentQuestion == totQuestions - 1) {
+    nextButton.textContent = "Finish";
+  }
+
+  if (currentQuestion == totQuestions) {
+    container.style.display = "none";
+    resultCont.style.display = "";
+    resultCont.textContent = "Your Score : " + score;
+    return;
+  }
+  loadQuestion(currentQuestion);
+}
+loadQuestion(currentQuestion);
+
+var startButton = document.getElementsByClassName("btn btn-danger");
+
+
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+startButton[0].addEventListener("click", function(){
+
+
+
+    var fiveMinutes = 60 * 5,
+    display = document.querySelector('#time');
+    startTimer(fiveMinutes, display);
+
+})
+
+
    
-   
-   
-    // 
+  /* var currentQuestion = 0;
+   var score = 0;
+   var questionEl= document.getElementById();
+   var a = document.getElementById();
+   var b = document.getElementById();
+   var c = document.getElementById();
+    var nextButton = document.getElementById();
+    varresultCont= document.getElementById();
 
 
 
